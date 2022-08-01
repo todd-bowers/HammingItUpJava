f8b3a66ecadb757a8161d3a281adf3ced6140bd1
todd-bowers <toddbows@gmail.com>
2022-08-01 15:43:10 -0400

    All tests pass

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,25 @@
 package zipcode;
 
 public class Hamming {
+    public String original;
+    public String mutated;
     public Hamming(String s, String s1) {
+        this.original = s;
+        this.mutated = s1;
+        checkStringLength();
     }
 
     public int getHammingDistance() {
-        return -1;
+        int distance = 0;
+        for (int i = 0; i < original.length(); i++) {
+            if (original.charAt(i) != mutated.charAt(i)) distance++;
+        }
+        return distance;
+    }
+
+    public void checkStringLength() throws IllegalArgumentException {
+        if (original.length() != mutated.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 }
